e16d435822532ee6bbcc363ec86493f9f9a6f70f
kpose <ganihujude@gmail.com>
2020-09-02 18:02:01 +0100

    sign up input validation

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -63,6 +63,17 @@ app.post('/post', (req, res) => {
       })
 })
 
+const isEmpty = (string) => {
+  if(string.trim() === '') return true;
+  else return false;
+}
+
+
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+}
 
 
 //sign up
@@ -74,6 +85,21 @@ app.post('/signup', (req, res) => {
     handle: req.body.handle,
   };
 
+
+  let errors = {};
+
+  if(isEmpty(newUser.email)) {
+    errors.email= "Must not be empty"
+  } else if (!isEmail(newUser.email)){
+    errors.email = 'Must be a valid email address'
+  }
+
+  if(isEmpty(newUser.password)) errors.password = "Must not be empty";
+  if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = "Passwords do not match";
+  if(isEmpty(newUser.handle)) errors.handle = "Must not be empty"
+
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors)
   //todo: validate data
   let token, userId;
   db.doc(`/users/${newUser.handle}`).get()
