6b9a708540406c81995fdedc7e7bbe9bdf8fb748
kpose <ganihujude@gmail.com>
2020-09-02 17:41:31 +0100

    sign up api and validation

---
+++ functions/dbschema.js
@@ -0,0 +1,11 @@
+let db ={
+    posts: [
+        {
+            userHandle: 'user',
+            body: "post body",
+            createdAt: "2020-09-02T14:31:15.379Z",
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,48 +1,58 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
+const app = require('express')();
+
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const config = {
+  apiKey: "AIzaSyArVzy0AW4x1Ih3Cz3VNwe3N_kU61GQ_18",
+    authDomain: "eolas-90876.firebaseapp.com",
+    databaseURL: "https://eolas-90876.firebaseio.com",
+    projectId: "eolas-90876",
+    storageBucket: "eolas-90876.appspot.com",
+    messagingSenderId: "437278046148",
+    appId: "1:437278046148:web:f12e44701565394a5fd691",
+    measurementId: "G-9408BS3QYR"
+}
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+const firebase = require('firebase');
+firebase.initializeApp(config);
 
+const db = admin.firestore();
+
+
+ 
 //Function to get all documents
-exports.getPosts = functions.https.onRequest((req, res) => {
-  admin
-    .firestore()
+app.get('/posts', (req, res) => {
+  db
     .collection("posts")
+    .orderBy('createdAt', 'desc')
     .get()
     .then((data) => {
       let posts = [];
       data.forEach((doc) => {
-        posts.push(doc.data());
+        posts.push({
+          postId: doc.id,
+          ...doc.data()
+        });
       });
       return res.json(posts);
     })
     .catch((err) => console.error(err));
-});
+})
 
 
 //functions to create new documents(posts)
 
-exports.createPost = functions.https.onRequest((req, res) => {
-  if(req.method !== 'POST'){
-    return res.status(400).json({error: 'Method not allowed'})
-  }
+app.post('/post', (req, res) => {
   const newPost = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   };
 
-  admin.firestore()
-      .collection('posts')
+  db.collection('posts')
       .add(newPost)
       .then(doc => {
         res.json({ message: `docment ${doc.id} created successfully`});
@@ -53,3 +63,59 @@ exports.createPost = functions.https.onRequest((req, res) => {
       })
 })
 
+
+
+//sign up
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  //todo: validate data
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`).get()
+    .then(doc => {
+      if(doc.exists) {
+        return res.status(400).json({ handle: "This Handle is already taken"});
+      } else {
+         return firebase
+                  .auth()
+                  .createUserWithEmailAndPassword(newUser.email, newUser.password)
+      }
+    })
+    .then(data => {
+      userId = data.user.uid;
+       return data.user.getIdToken()
+    })
+    .then(idToken => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId
+      };
+
+      db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch(err => {
+      console.error(err)
+      if (err.code === 'auth/email-already-in-use') {
+        return res.status(400).json({email: "Email already in use" });
+      } else {
+        return res.status(500).json({error: err.code});
+      }
+    })
+
+
+  
+})
+
+
+exports.api = functions.region('europe-west1').https.onRequest(app);
