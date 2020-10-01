9d34683952d1aa0529d91cb570a4f55d7602ddf7
kpose <ganihujude@gmail.com>
2020-10-01 17:40:18 +0100

    load posts from api

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, Text, View, Dimensions} from 'react-native';
 
 export default function HomeScreen() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch('https://europe-west1-eolas-90876.cloudfunctions.net/api/posts')
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  let recentPosts = data ? (
+    data.map((post) => <Text key={post.postId}>{post.body}</Text>)
+  ) : (
+    <Text> loading.....</Text>
+  );
+
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text style={{color: 'red'}}>HOME SCREEN</Text>
+
+      {recentPosts}
     </View>
   );
 }
